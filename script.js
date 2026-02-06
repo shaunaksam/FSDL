222eb4e80e378526639870e379fad3bf005854ac
tushargittemit <tushar.gitte@mitwpu.edu.in>
2026-02-06 10:28:06 +0530

    Assignment 3: added typewriter effect for name

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Assignment 3 JS is working!");
+// Typewriter effect 
+const nameText = "Shaunak Samant";
+const nameElement = document.querySelector("header h1");
+let idx = 0;
+
+nameElement.innerHTML = "";
+
+function typeWriter() {
+  if (idx < nameText.length) {
+    nameElement.innerHTML += nameText.charAt(idx);
+    idx++;
+    setTimeout(typeWriter, 150); 
+  }
+}
+
+typeWriter();
